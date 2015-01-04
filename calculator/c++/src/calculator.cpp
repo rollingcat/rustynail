a57e1f28a6812be4a33a51d73625f3a1b1373d9a
rollingcat <everydaycoding@gmail.com>
2015-01-04 22:08:32 +0900

    Big Integer의 경우 string을 input으로 + - * 연산 - 음수가 포함된 + - 연산을 할때 버그 있음

--- calculator/c++/src/calculator.cpp
+++ calculator/c++/src/calculator.cpp
@@ -6,6 +6,10 @@
 #include <string>
 #include <vector>
 #include <cstdlib>
+#include <algorithm>
+#include <assert.h>
+
+#define MAX_DIGIT_NUM 10
 
 using namespace std;
 
@@ -35,10 +39,6 @@ void Tokenize(const string& input) {
         if (prev < line.length())
             tokens.push_back(line.substr(prev, string::npos));
     }
-
-//    for (size_t i = 0; i < tokens.size(); ++i) {
-//        cout << tokens[i] << endl;
-//    }
 }
 
 string ConvertInfixToPostfix() {
@@ -96,6 +96,248 @@ string ConvertInfixToPostfix() {
     return postfix.str();
 }
 
+enum Comparing {
+    Less,
+    Greater,
+    Equal
+};
+
+Comparing Compare(const string& a, const string& b) {
+    if (a.length() > b.length())
+        return Greater;
+    else if (a.length() < b.length())
+        return Less;
+
+    for (size_t i = 0; i < a.length(); ++i) {
+        if (a[i] > b[i])
+            return Greater;
+        else if (a[i] < b[i])
+            return Less;
+    }
+    return Equal;
+}
+
+string Add(const string& a, const string& b)
+{
+    int firstNum, secondNum;
+    istringstream(a) >> firstNum;
+    istringstream(b) >> secondNum;
+    int newNum = firstNum + secondNum;
+
+    stringstream ss;
+    ss << newNum;
+    return ss.str();
+}
+
+string AddBigInt(const string& aa, const string& bb) {
+    string a = aa;
+    std::reverse(a.begin(), a.end());
+    string b = bb;
+    std::reverse(b.begin(), b.end());
+
+    string result;
+    size_t aLen = a.length();
+    size_t bLen = b.length();
+    size_t addLen = (aLen >= bLen) ? aLen : bLen;
+
+    char transfer = 0;
+    for (size_t i = 0; i < addLen; ++i) {
+        char aChar, bChar;
+        aChar = (i >= aLen) ? 0 : a[i] - '0';
+        bChar = (i >= bLen) ? 0 : b[i] - '0';
+
+        char sum = aChar + bChar + transfer;
+        transfer = sum / 10;
+        char num = sum % 10;
+        result.insert(0, 1, num + '0');
+    }
+    if (transfer != 0) {
+        result.insert(0, 1, transfer + '0');
+    }
+    return result;
+}
+
+string Subtract(const string& a, const string& b) {
+    int firstNum, secondNum;
+    istringstream(a) >> firstNum;
+    istringstream(b) >> secondNum;
+    int newNum = firstNum - secondNum;
+
+    stringstream ss;
+    ss << newNum;
+    return ss.str();
+}
+
+string SubtractBigInt(const string& aa, const string& bb) {
+    string a, b;
+
+    Comparing comp = Compare(aa, bb);
+    if (comp == Greater) {
+        a = aa;
+        b = bb;
+    } else if (comp == Less) {
+        a = bb;
+        b = aa;
+    } else
+        return string("0");
+
+    std::reverse(a.begin(), a.end());
+    std::reverse(b.begin(), b.end());
+
+    string result;
+    size_t aLen = a.length();
+    size_t bLen = b.length();
+    size_t addLen = (aLen >= bLen) ? aLen : bLen;
+
+    char borrow = 0;
+    for (size_t i = 0; i < addLen; ++i) {
+        char aChar, bChar;
+        aChar = (i >= aLen) ? 0 : a[i] - '0';
+        bChar = (i >= bLen) ? 0 : b[i] - '0';
+
+        int sub = aChar - bChar - borrow;
+        if (sub < 0) {
+            borrow = 1;
+            sub += 10;
+        } else
+            borrow = 0;
+
+        char ch = sub + '0';
+        result.append(&ch, 1);
+    }
+
+    while (result.back() == '0')
+        result.pop_back();
+
+    std::reverse(result.begin(), result.end());
+
+    if (comp == Less)
+        result.insert(0, 1, '-');
+    return result;
+}
+
+string Multiply(const string& a, const string& b) {
+    unsigned int firstNum, secondNum;
+    istringstream(a) >> firstNum;
+    istringstream(b) >> secondNum;
+    unsigned int newNum = firstNum * secondNum;
+
+    stringstream ss;
+    ss << newNum;
+    return ss.str();
+}
+
+void Normalize(vector<int>& input) {
+    input.push_back(0);
+
+    for (size_t i = 0; i < input.size(); ++i) {
+        if (input[i] == 0) {
+            continue;
+        }
+        input[i + 1] += input[i] / 10;
+        input[i] %= 10;
+    }
+    while (input.back() == 0)
+        input.pop_back();
+}
+
+string MultplyBigIntBruteForce(const string& aa, const string& bb) {
+    string a = aa;
+    string b = bb;
+    reverse(a.begin(), a.end());
+    reverse(b.begin(), b.end());
+
+    bool aMinus = a.back() == '-';
+    bool bMinus = b.back() == '-';
+    if (aMinus) a.pop_back();
+    if (bMinus) b.pop_back();
+
+    bool minus = false;
+    if (aMinus != bMinus)
+        minus = true;
+
+    vector<int> result(a.length() + b.length() + 1, 0);
+    for (size_t i = 0; i < a.length(); ++i) {
+        for (size_t j = 0; j < b.length(); ++j) {
+            result[i+j] += (a[i] - '0') * (b[j] - '0');
+        }
+    }
+    Normalize(result);
+
+    string ret;
+    ret.reserve(result.size() + 1);
+    if (minus)
+        ret.append('-', 1);
+
+    char ch = 0;
+    for (vector<int>::iterator iter = result.end() - 1; iter != result.begin(); --iter) {
+        ch = *iter + '0';
+        ret.append(&ch, 1);
+    }
+    ch = *result.begin() + '0';
+    ret.append(&ch, 1);
+    return ret;
+}
+
+string MultiplyBigInt(const string& a, const string& b) {
+    return MultplyBigIntBruteForce(a, b);
+}
+
+bool RPNWithBigIntegers(const string& input, string& result) {
+    istringstream iss(input);
+    vector<string> stack;
+    string token;
+    while (iss >> token) {
+        if (token.length() == 1 && !std::isdigit(token[0])) {
+            size_t stackSize = stack.size();
+            if (stackSize < 2) {
+                cout << "Invalid postfix notation!" << endl;
+                return false;
+            }
+            string& secondNum = stack[stackSize - 1];
+            string& firstNum = stack[stackSize - 2];
+
+            int secondLen = secondNum.length();
+            int firstLen = firstNum.length();
+
+            string newNum;
+            if (token == "+") {
+                if (firstLen >= (MAX_DIGIT_NUM - 1) || secondLen >= (MAX_DIGIT_NUM - 1)) {
+                    newNum = AddBigInt(firstNum, secondNum);
+                } else {
+                    newNum = Add(firstNum, secondNum);
+                }
+            } else if (token == "-") {
+                if (firstLen >= (MAX_DIGIT_NUM - 1) || secondLen >= (MAX_DIGIT_NUM - 1)) {
+                    newNum = SubtractBigInt(firstNum, secondNum);
+                } else {
+                    newNum = Subtract(firstNum, secondNum);
+                }
+            } else if (token == "*") {
+                if ((firstLen + secondLen) > MAX_DIGIT_NUM) {
+                    newNum = MultiplyBigInt(firstNum, secondNum);
+                } else {
+                    newNum = Multiply(firstNum, secondNum);
+                }
+            } else {
+                cout << "Unsupported operator: " << token << endl;
+                exit(0);
+            }
+            stack.pop_back();
+            stack.pop_back();
+            stack.push_back(newNum);
+        } else {
+            stack.push_back(token);
+        }
+    }
+    if (stack.size() != 1) {
+        cout << "Invalid postfix notation!" << endl;
+        return false;
+    }
+    result = stack.back();
+    return true;
+}
+
 bool RPN(const string& input, int& result) {
     istringstream iss(input);
     vector<int> stack;
@@ -140,6 +382,18 @@ bool RPN(const string& input, int& result) {
 int main() {
     cout << "<Calculator>" << endl;
 
+    string ret1 = Add("111111111", "9");
+    string ret2 = AddBigInt("111111111", "9");
+    assert(ret1 == ret2);
+
+    ret1 = Subtract("123456789", "987654321");
+    ret2 = SubtractBigInt("123456789", "987654321");
+    assert(ret1 == ret2);
+
+    ret1 = Multiply("12345", "65432");
+    ret2 = MultiplyBigInt("12345", "65432");
+    assert(ret1 == ret2);
+
     const char *tests[] = {
         "3 + 4 * 2 - ( 1 - 5 ) + 7",    /* RC mandated: OK */
         "123",                          /* OK */
@@ -163,8 +417,8 @@ int main() {
 
         cout << "postfix: " << postfix << endl;
 
-        int result = 0;
-        if (RPN(postfix, result)) {
+        string result;
+        if (RPNWithBigIntegers(postfix, result)) {
             cout << "result: " << result << endl;
         }
     }
