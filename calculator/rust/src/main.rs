9f2c8de29aceddf3e9b3a035c0c18a5ce734f3c9
rollingcat <everydaycoding@gmail.com>
2015-01-05 15:23:06 +0900

    [Rust] string을 입력으로 add, subtract, multiply 수행 - string을 int로 변환하므로 big int는 처리하지 못함

--- calculator/rust/src/main.rs
+++ calculator/rust/src/main.rs
@@ -33,7 +33,7 @@ fn main() {
 	  		println!("");
 	  	}
 
-	  	let result = rpn(postfix);
+		let result = rpn_with_big_int(postfix);
 	  	match result {
 	  		Some(x) => println!("result: {}", x),
 	  		None => println!("None"),
@@ -75,6 +75,63 @@ fn rpn(input: Vec<&str>) -> Option<int> {
 	Some(*stack.last().unwrap())
 }
 
+fn add(a: &str, b: &str) -> String {
+	let a_num = a.parse::<int>();
+	let b_num = b.parse::<int>();
+	let new_num = a_num.unwrap() + b_num.unwrap();
+	new_num.to_string()
+}
+
+fn subtract(a: &str, b: &str) -> String {
+	let a_num = a.parse::<int>();
+	let b_num = b.parse::<int>();
+	let new_num = a_num.unwrap() - b_num.unwrap();
+	new_num.to_string()
+}
+
+fn multiply(a: &str, b: &str) -> String {
+	let a_num = a.parse::<int>();
+	let b_num = b.parse::<int>();
+	let new_num = a_num.unwrap() * b_num.unwrap();
+	new_num.to_string()
+}
+
+fn rpn_with_big_int(input: Vec<&str>) -> Option<String> {
+	let mut stack: Vec<String> = Vec::new();
+
+	for token in input.iter() {
+		let ch = token.char_at(0);
+		if token.len() == 1 && !UnicodeChar::is_numeric(ch) {
+			if stack.len() < 2 {
+				println!("Invalid postfix notation");
+				return None;
+			}
+			let second_num = stack.pop().unwrap();
+			let first_num = stack.pop().unwrap();
+
+			let new_num;
+			if ch == '+' {
+				new_num = add(first_num.as_slice(), second_num.as_slice());
+			} else if ch == '-' {
+				new_num = subtract(first_num.as_slice(), second_num.as_slice());
+			} else if ch == '*' {
+				new_num = multiply(first_num.as_slice(), second_num.as_slice());
+			} else {
+				println!("Unsupported operator");
+				return None;
+			}
+			stack.push(new_num);
+		} else {
+			stack.push(token.to_string());
+		}
+	}
+	if stack.len() != 1 {
+		println!("Invalid postfix notation");
+		return None;
+	}
+	stack.pop()
+}
+
 fn convert_to_postfix(input: Vec<&str>) -> Vec<&str> {
 	let mut postfix: Vec<&str> = Vec::new();
 	let mut stack: Vec<int> = Vec::new();
@@ -104,7 +161,7 @@ fn convert_to_postfix(input: Vec<&str>) -> Vec<&str> {
 					}
 					stack.push(idx);
 				}
-			} else if ch== '(' {
+			} else if ch == '(' {
 				stack.push(-2i);
 			} else if ch == ')' {
 				while *stack.last().unwrap() != -2i {
